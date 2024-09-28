54da348cd15912a98944bce9037e56c3582ee2ec
Tizis <tizisdgk@gmail.com>
2024-09-28 05:00:48 +0300

    Better Laser

--- Assets/Internal Assets/Scripts/Enemies/ShotgunEnemy.cs
+++ Assets/Internal Assets/Scripts/Enemies/ShotgunEnemy.cs
@@ -7,16 +7,13 @@ public class ShotgunEnemy : ShootingEnemy {
     [SerializeField]
     private float _sideBulletsAngle = 30f;
 
-    protected override IEnumerator Attack() {
-        IsAttacking = true;
+    protected override void Shoot() {
         //_animator.SetBool("IsAttacking", true);
 
         ChooseBulletDirection(Direction);
         ChooseBulletDirection(Quaternion.Euler(0, 0, _sideBulletsAngle) * (PlayerPosition.position - transform.position).normalized);
         ChooseBulletDirection(Quaternion.Euler(0, 0, -_sideBulletsAngle) * (PlayerPosition.position - transform.position).normalized);
 
-        yield return new WaitForSeconds(_cooldown);
-        IsAttacking = false;
         //_animator.SetBool("IsAttacking", false);
     }
 
@@ -24,5 +21,6 @@ public class ShotgunEnemy : ShootingEnemy {
     {
         GameObject bulletPrefab = Instantiate(BulletPrefab, transform.position, transform.rotation);
         bulletPrefab.GetComponent<Rigidbody2D>().AddForce(direction * _bulletForce, ForceMode2D.Impulse);
+        lastShotTime = Time.time;
     }
 }
